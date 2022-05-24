bd4025684ce21d98c511718d0e6676dd281f83cf
mariapivnova <mariapivnova@yandex.ru>
2022-05-24 10:30:20 +0300

    commit

--- app/src/main/java/com/example/tablogenshin/MainActivity.java
+++ app/src/main/java/com/example/tablogenshin/MainActivity.java
@@ -10,10 +10,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Integer venty = 0;
-    private Integer xily = 0;
-    private TextView ventyTextView;
-    private TextView xilyTextView;
+    private Integer winx = 0;
+    private Integer trix = 0;
+    private TextView winxTextView;
+    private TextView trixTextView;
 
     private static final String TAG = "MainActivity";
 
@@ -29,26 +29,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ventyTextView = findViewById(R.id.venty_score);
-        xilyTextView = findViewById(R.id.xily_score);
+        winxTextView = findViewById(R.id.winx_score);
+        trixTextView = findViewById(R.id.trix_score);
     }
 
     @SuppressLint("SetTextI18n")
-    public void onClickAddVenty(View view) {
-        venty++;
-        ventyTextView.setText(venty.toString());
+    public void onClickAddWinx(View view) {
+        winx++;
+        winxTextView.setText(winx.toString());
     }
 
     @SuppressLint("SetTextI18n")
-    public void onClickAddXily(View view) {
-        xily++;
-        xilyTextView.setText(xily.toString());
+    public void onClickAddTrix(View view) {
+        trix++;
+        trixTextView.setText(trix.toString());
     }
 
     @SuppressLint("SetTextI18n")
     public void onClickButtonClear(View view) {
-        venty = 0;
-        xily = 0;
+        winx = 0;
+        trix = 0;
 
         resetUI();
     }
@@ -56,27 +56,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("venty", venty);
-        outState.putInt("xily", xily);
+        outState.putInt("winx", winx);
+        outState.putInt("trix", trix);
         Log.d(TAG, "onSaveInstanceState");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        if (savedInstanceState != null && savedInstanceState.containsKey("venty")) {
-            venty= savedInstanceState.getInt("venty");
+        if (savedInstanceState != null && savedInstanceState.containsKey("winx")) {
+            winx= savedInstanceState.getInt("winx");
         }
-        if ((savedInstanceState != null && savedInstanceState.containsKey("xily"))) {
-            xily= savedInstanceState.getInt("xily");
+        if ((savedInstanceState != null && savedInstanceState.containsKey("trix"))) {
+            trix= savedInstanceState.getInt("trix");
         }
         Log.d(TAG, "onRestoreInstanceState");
     }
 
     @SuppressLint("SetTextI18n")
     private void resetUI() {
-        ventyTextView.setText(venty.toString());
-        xilyTextView.setText(xily.toString());
+        winxTextView.setText(winx.toString());
+        trixTextView.setText(trix.toString());
         Log.d(TAG, "resetUI");
     }
 }
